70c8f44e13ef26c3388078d8189f8f232350642c
isdanielt <daniel29t04@hotmail.com>
2022-11-01 19:50:04 -0400

    Parte-1

--- Clase_5_2022T4/index.js
+++ Clase_5_2022T4/index.js
@@ -5,11 +5,11 @@ let app = express()
 //app.get('/', function(req, res){
   //  res.send("hello word")
 //})
-
+app.use(express.static(__dirname + "/aplicacion"))
 app.get('/', function(req, res){
     res.sendFile(__dirname + "/aplicacion/index.html")
 })
-
+ 
 app.listen(3001)
 
 console.log("este es mi servidor: http://localhost:3001")
